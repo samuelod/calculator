d77e0266079b731a01eb10ebd681f5410e27746e
Sam Odulaja <samuelod@buffalo.edu>
2023-03-20 11:07:52 -0400

    made style changes & applied onclick method

--- main.js
+++ main.js
@@ -1,5 +1,9 @@
 function getInputValue(){
-    let input = Event.target.innerText;
+    let input = event.target.innerText;
     console.log(input)
 }
 
+let buttons = document.getElementsByTagName('button');
+for(var i = 0; i<buttons.length;i++){
+    buttons[i].setAttribute('onclick','getInputValue()');//with this we will add the onclick method to all the buttons
+}
