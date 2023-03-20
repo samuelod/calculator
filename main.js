fc16306d940197328ad1c7f87b417e3b617572dc
Sam Odulaja <samuelod@buffalo.edu>
2023-03-20 12:26:37 -0400

    using eval function to return a result

--- main.js
+++ main.js
@@ -17,7 +17,27 @@ function printValue(val){
         }
 
     }else{
+        if(val =="DEL"){
+            output.innerHTML = current.slice(0,-1); // the slice() method allows us to remove a part of the string 
+            if(output.innerHTML.length <=1){
+                output.innerHTML = "0";
+            }
+        }
 
+        if(val != "AC" && val!="DEL" && val!="="){
+            output.innerHTML += val;
+        }
+
+        if(val =="AC"){
+            output.innerHTML = "0";
+         }
+
+         //now we will return the formula 
+         //for this we will use the eval function that return a result of a mathematic formula
+         if(val =="="){
+            let ans = output.innerHTML
+            output.innerHTML = eval(ans)
+         }
     }
 }
 
